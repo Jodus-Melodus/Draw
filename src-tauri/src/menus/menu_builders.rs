2efc0213e8b8af894e476de06bd2032a2446d82e
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-25 08:59:29 +0200

    remove input device selection and move output device selection to file menu

--- src-tauri/src/menus/menu_builders.rs
+++ src-tauri/src/menus/menu_builders.rs
@@ -20,9 +20,30 @@ fn build_file_menu(app: &App<Wry>) -> Submenu<Wry> {
         .build(app)
         .unwrap();
 
+    let audio_context = app.state::<states::StateAudioContext>();
+    let output_device_registry = audio_context.output_device_registry.clone();
+
+    let output_menu = SubmenuBuilder::new(app, "Output Device")
+        .id("file-output-devices")
+        .build()
+        .unwrap();
+
+    for (i, output_device_name) in output_device_registry.list().iter().enumerate() {
+        let id = format!("file-output-device-{}", i);
+        let output_device = CheckMenuItemBuilder::new(output_device_name)
+            .id(id)
+            .checked(i == 0)
+            .build(app)
+            .unwrap();
+
+        output_menu
+            .append(&output_device)
+            .expect("Failed to add item to menu");
+    }
+
     let file_menu = SubmenuBuilder::new(app, "File")
         .id("file")
-        .item(&settings)
+        .items(&[&settings, &output_menu])
         .quit()
         .build()
         .unwrap();
@@ -56,63 +77,11 @@ fn build_project_menu(app: &App<Wry>) -> Submenu<Wry> {
     project_menu
 }
 
-fn build_device_menu(app: &App<Wry>) -> Submenu<Wry> {
-    let audio_context = app.state::<states::StateAudioContext>();
-    let input_device_registry = audio_context.input_device_registry.clone();
-    let output_device_registry = audio_context.output_device_registry.clone();
-
-    let input_menu = SubmenuBuilder::new(app, "Input Device")
-        .id("devices-input-devices")
-        .build()
-        .unwrap();
-
-    for (i, input_device_name) in input_device_registry.list().iter().enumerate() {
-        let id = format!("devices-input-{}", i);
-        let input_device = CheckMenuItemBuilder::new(input_device_name)
-            .id(id)
-            .checked(i == 0)
-            .build(app)
-            .unwrap();
-
-        input_menu
-            .append(&input_device)
-            .expect("Failed to add item to menu");
-    }
-
-    let output_menu = SubmenuBuilder::new(app, "Output Device")
-        .id("devices-output-devices")
-        .build()
-        .unwrap();
-
-    for (i, output_device_name) in output_device_registry.list().iter().enumerate() {
-        let id = format!("devices-output-{}", i);
-        let output_device = CheckMenuItemBuilder::new(output_device_name)
-            .id(id)
-            .checked(i == 0)
-            .build(app)
-            .unwrap();
-
-        output_menu
-            .append(&output_device)
-            .expect("Failed to add item to menu");
-    }
-
-    let device_menu = SubmenuBuilder::new(app, "Devices")
-        .id("devices")
-        .item(&input_menu)
-        .item(&output_menu)
-        .build()
-        .unwrap();
-
-    device_menu
-}
-
 pub fn build_menus(app: &App<Wry>) -> Menu<Wry> {
     let file_menu = build_file_menu(app);
     let project_menu = build_project_menu(app);
-    let device_menu = build_device_menu(app);
     MenuBuilder::new(app)
-        .items(&[&file_menu, &project_menu, &device_menu])
+        .items(&[&file_menu, &project_menu])
         .build()
         .unwrap()
 }
@@ -126,17 +95,12 @@ pub async fn handle_menu_events(app: &AppHandle, event: &MenuEvent) {
         "file-settings" => pages::settings_page::open_settings(app),
         "project-add-track-file" => menus::project_menu::add_track_file(app).await,
         "project-add-track-stream" => pages::select_input_stream::open_select_input_stream(app),
-        _ => {
-            if id.starts_with("devices-input") {
-                update_device_index(audio_context.input_device_index.clone(), id);
-                update_radio_group_menu(app, id);
-            } else if id.starts_with("devices-output") {
-                update_device_index(audio_context.output_device_index.clone(), id);
-                update_radio_group_menu(app, id);
-            } else {
-                eprintln!("Unknown menu item selected");
-            }
+        _ if id.starts_with("file-output-device-") => {
+            update_device_index(audio_context.output_device_index.clone(), id);
+            update_radio_group_menu(app, id);
+            // TODO update master out track
         }
+        _ => eprintln!("Unknown menu item selected"),
     }
 }
 
@@ -159,14 +123,10 @@ fn update_radio_group_menu(app: &AppHandle, id: &str) {
 }
 
 fn loop_through_sub_menus(id: &str, main_menu: MenuItemKind<Wry>) {
-    if main_menu.id().0 == "devices" {
+    if main_menu.id().0 == "file" {
         if let MenuItemKind::Submenu(device_menu) = main_menu {
             for menu_item in device_menu.items().expect("Failed to get sub menu items") {
-                if menu_item.id().0 == "devices-input-devices" && id.starts_with("devices-input") {
-                    uncheck_other_devices(id, menu_item);
-                } else if menu_item.id().0 == "devices-output-devices"
-                    && id.starts_with("devices-output")
-                {
+                if menu_item.id().0 == "file-output-devices" && id.starts_with("file-output") {
                     uncheck_other_devices(id, menu_item);
                 }
             }
@@ -175,9 +135,9 @@ fn loop_through_sub_menus(id: &str, main_menu: MenuItemKind<Wry>) {
 }
 
 fn uncheck_other_devices(id: &str, menu_item: MenuItemKind<Wry>) {
-    if let MenuItemKind::Submenu(io_devices) = menu_item {
-        for io_device in io_devices.items().expect("Failed to get menus") {
-            if let MenuItemKind::Check(device) = io_device {
+    if let MenuItemKind::Submenu(o_devices) = menu_item {
+        for o_device in o_devices.items().expect("Failed to get menus") {
+            if let MenuItemKind::Check(device) = o_device {
                 device
                     .set_checked(device.id().0 == id)
                     .expect("Failed to uncheck item");
