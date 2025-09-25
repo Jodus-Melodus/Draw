64751a2143d7f53dbe01b8b09bd65b00ffaa1ab9
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-25 10:07:30 +0200

    put await in function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { getTrackList, updateTrack } from "./backend/tracks";
 
-let track_list = await getTrackList();
-console.log(track_list.tracks);
-await updateTrack("master-out", { Volume: 0.0 });
-let track_list2 = await getTrackList();
-console.log(track_list2.tracks);
+async function init() {
+    let track_list = await getTrackList();
+    console.log(track_list.tracks);
+    await updateTrack("master-out", { Volume: 0.0 });
+    let track_list2 = await getTrackList();
+    console.log(track_list2.tracks);
+}
+
+init();
