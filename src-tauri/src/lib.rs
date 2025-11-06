510fc62731a00c0c4dc580427f36c21e1e3a14d4
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-11-06 13:04:31 +0200

    moved discord rich presence to state mixer

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,5 @@
 use std::sync::{Arc, Mutex};
 
-use discord_rich_presence::{activity::Activity, DiscordIpc, DiscordIpcClient};
 use tauri::{async_runtime, generate_context, generate_handler, Builder, Manager, WindowEvent};
 
 mod menus;
@@ -11,20 +10,6 @@ mod types;
 
 #[cfg_attr(mobile, mobile_entry_point)]
 pub async fn run() {
-    // TODO move to state
-    let discord_rich_presence_client = Mutex::new(DiscordIpcClient::new("1435880809767637164"));
-
-    if let Ok(mut client) = discord_rich_presence_client.lock() {
-        client.connect().expect("Failed to connect to Discord");
-        client
-            .set_activity(
-                Activity::new()
-                    .state("DRAW")
-                    .details("Making noise with DRAW"),
-            )
-            .expect("Failed to set activity");
-    }
-
     let state_audio_context = project::states::StateAudioContext::new();
     let master_output_device = state_audio_context
         .output_device()
@@ -57,14 +42,15 @@ pub async fn run() {
             project::commands::load_project,
             project::commands::save_project,
         ))
-        .on_window_event(move|w, e| match e {
+        .on_window_event(move |w, e| match e {
             WindowEvent::CloseRequested { .. } => {
-                if let Ok(mut client) = discord_rich_presence_client.lock() {
-                    client.close().expect("Failed to close Discord connection");
-                }
-
                 if w.label() == "main" {
                     let app_handle = w.app_handle();
+                    let state_mixer_guard = app_handle.state::<project::states::StateMixerGuard>();
+                    if let Ok(state_mixer) = state_mixer_guard.0.lock() {
+                        state_mixer.disconnect_from_discord();
+                    }
+
                     if let Some(win) = app_handle.get_webview_window("settings") {
                         let _ = win.close();
                     }
